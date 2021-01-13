4a08391caa991f42570d7007c2ccbf8d2b62338c
mike aspinall <mbaspinall@gmail.com>
2021-01-13 13:08:23 -0500

    third commit

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,7 +1,12 @@
 //app is the entry point of application
 
-function sendmsg(){
-console.log("Hello World")
-}
+var http = require('http')
 
-sendmsg()
+http.createServer(function(request, response){
+    //http header
+    response.writeHead(200,{'content-type':'text/plain'})
+    //Send a response to the body of the html
+    response.end("Wouldn't you like to be a pepper too???")
+}).listen(3000)
+
+console.log("Server is running on port 3000")
